35a968760607b3a821d0684d9239e6c7592b1c30
Whyll <whylgher@gmail.com>
2022-11-30 14:05:05 +0000

    Criando classes de conexão com o banco de dados

--- lib/application/config/application_config.dart
+++ lib/application/config/application_config.dart
@@ -1,12 +1,23 @@
+import 'package:cuidapet_api/application/config/database_connection_configuration.dart';
 import 'package:dotenv/dotenv.dart' show load, env;
+import 'package:get_it/get_it.dart';
 
 class ApplicationConfig {
   Future<void> loadConfigApplication() async {
     await _loadEnv();
-    final variavel = env['url_banco_de_dados'];
-
-    print(variavel);
+    _loadDataBaseConfig();
   }
 
   Future<void> _loadEnv() async => load();
+
+  void _loadDataBaseConfig() {
+    final databaseConfig = DatabaseConnectionConfiguration(
+      host: env['DATABASE_HOST'] ?? env['databaseHost']!,
+      user: env['DATABASE_USER'] ?? env['databaseUser']!,
+      port: int.tryParse(env['DATABASE_PORT'] ?? env['databasePort']!) ?? 0,
+      password: env['DATABASE_PASSWORD'] ?? env['databasePassword']!,
+      databaseName: env['DATABASE_NAME'] ?? env['databaseName']!,
+    );
+    GetIt.I.registerSingleton(databaseConfig);
+  }
 }
---
+++ lib/application/database/i_database_connection_impl.dart
@@ -0,0 +1,23 @@
+import 'package:cuidapet_api/application/config/database_connection_configuration.dart';
+import 'package:mysql1/mysql1.dart';
+
+import './i_database_connection.dart';
+
+class IDatabaseConnectionImpl implements IDatabaseConnection {
+  final DatabaseConnectionConfiguration _configuration;
+
+  IDatabaseConnectionImpl(
+    this._configuration,
+  );
+
+  @override
+  Future<MySqlConnection> openConnection() {
+    return MySqlConnection.connect(ConnectionSettings(
+      host: _configuration.host,
+      port: _configuration.port,
+      user: _configuration.user,
+      password: _configuration.password,
+      db: _configuration.databaseName,
+    ));
+  }
+}
